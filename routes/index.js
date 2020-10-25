04b103d0213ded5a9a7adddeaa1a6031ff106976
elee-tudublin <55653651+elee-tudublin@users.noreply.github.com>
2020-10-25 13:22:32 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -12,41 +12,5 @@ router.get('/', function (req, res) {
 
 });
 
-// Calculator
-// Acccepts two paramters via the url querystring
-// example: http://localhost:8080/calc?a=3&b=2
-
-router.get('/calc', function (req, res) {
-
-    // Declare variables
-    let numA = "";
-    let numB = "";
-
-    // Validate input - important as a bad input could crash the server or lead to an attack
-    // check that params exist, if they do assign their values to the variables
-    if (typeof req.query.a != 'undefined')
-        numA = req.query.a;
-
-    if (typeof req.query.b != 'undefined')    
-        numB = req.query.b;
-
-    // See link to validator npm package (at top) for doc.
-    // If validation fails return an error message
-    if (!validator.isNumeric(numA, { no_symbols: true }) || !validator.isNumeric(numB, { no_symbols: true })) {
-        res.statusMessage = "Bad Request";
-        res.status(400).end();
-        return false;
-    }
-    // Note paramters are passed as strings to convert to numbers
-    const answer = Number(numA) + Number(numB);
-
-    // Send a JSON response - this app will be a web api so no need to send HTML
-    //res.end(JSON.stringify({message: 'This is the home page'}));
-    res.json({answer: `${numA} + ${numB} = ${answer}`});
-
-});
-
-
-
 // export
 module.exports = router;
