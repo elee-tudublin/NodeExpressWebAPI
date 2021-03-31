a02c5ae71e70c47b5e0c30f64af7343594c16f88
elee-tudublin <55653651+elee-tudublin@users.noreply.github.com>
2021-03-31 12:03:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // require imports packages required by the application
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors')
+const helmet = require('helmet');
+const morgon = require('morgan');
 
-const HOST = '127.0.0.1';
+const HOST = '0.0.0.0';
 const PORT = 8080;
+
 
 // app is a new instance of express (the web app framework)
 let app = express();
+
+// adding Helmet to enhance your API's security
+//app.use(helmet());
 
 // Application settings
 app.use((req, res, next) => {
@@ -16,17 +21,24 @@ app.use((req, res, next) => {
     next();
 }); 
 
-// Allow app to support differnt body content types (using the bidyParser package)
-app.use(bodyParser.text());
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support url encoded bodies
+// Logging
+app.use(morgon('combined'));
+
+// Allow app to support differnt body content types (using the bodyParser package)
+app.use(express.text());
+// support json encoded bodies
+app.use(express.json());
+// support url encoded bodies
+app.use(express.urlencoded({
+    extended: true
+}));
 
 // cors
 // https://www.npmjs.com/package/cors
 // https://codesquery.com/enable-cors-nodejs-express-app/
 // Simple Usage (Enable All CORS Requests)
-app.use(cors());
-app.options('*', cors()) // include before other routes
+// app.use(cors());
+// app.options('*', cors()) // include before other routes
 
 /* Configure app Routes to handle requests from browser */
 // The home page 
@@ -42,8 +54,8 @@ app.use(function (req, res, next) {
 
 // Start the HTTP server using HOST address and PORT consts defined above
 // Lssten for incoming connections
-var server = app.listen(PORT, HOST, function() {
-    console.log(`Express server listening on http://localhost:${PORT}`);
+var server = app.listen(PORT, HOST, () => {
+    console.log(`Express server listening on http://${HOST}:${PORT}`);
 });
 
 // export this as a module, making the app object available when imported.
